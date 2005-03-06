830022bc0bc14c20ff9001ef27fa08bbb10c8286
paolo <paolo@5d9bfbcb-4a49-0410-acfb-394a87e4f62c>
2005-03-06 23:11:03 +0000

    Uri base modification (added counter)

--- welkin/trunk/src/edu/mit/simile/welkin/resource/PartialUri.java
+++ welkin/trunk/src/edu/mit/simile/welkin/resource/PartialUri.java
@@ -4,12 +4,22 @@ import java.awt.Color;
 
 
 public class PartialUri {
+	private int count;
 	private String base;
 	public Color color;
 	
 	public PartialUri(String base, Color color) {
+		this.count = 1;
 		this.base = base;
 		this.color = color;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void incCount() {
+		count++;
 	}
 	
 	public String getBase() {
