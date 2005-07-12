034412bc920c87d0c4321e063454531209e796b4
stefano <stefano@5d9bfbcb-4a49-0410-acfb-394a87e4f62c>
2005-07-12 23:00:19 +0000

    tagging

---
+++ welkin/tags/1.1/src/edu/mit/simile/welkin/InDegreeChart.java
@@ -0,0 +1,26 @@
+package edu.mit.simile.welkin;
+
+import java.util.Iterator;
+
+import edu.mit.simile.welkin.ModelCache.WResource;
+import edu.mit.simile.welkin.ModelCache.WStatement;
+
+public class InDegreeChart extends ModelChart {
+
+    public InDegreeChart(ModelManager model) {
+        super(model);
+    }
+
+    public int process(WResource node) {
+        int counter = 0;
+        for (Iterator it = node.linkedSubjectNodes.iterator(); it.hasNext();) {
+            Object o = it.next();
+            if (o instanceof WStatement) {
+                WResource n = ((WStatement) o).subject;
+                if (n.isVisible()) counter++;
+            }
+        }
+        return counter;
+    }
+}
+
